7d52de967bfb3302e24ad72411e1e1abb336087d
gaston coria <coria.cs@gmail.com>
2021-10-09 12:56:59 -0300

    termine ejer3

--- EjerciciosComplementariosLevel1/Ejercicio3.java
+++ EjerciciosComplementariosLevel1/Ejercicio3.java
@@ -6,15 +6,25 @@ public class Ejercicio3 {
 
     public static void main (String[] args){
 
+
+
         Scanner scan = new Scanner(System.in);
+
+        System.out.print("Introduce un numero: ");
 
         int numero = scan.nextInt();
 
-        System.out.println("Introduce un numero: ");
-
         for(int i=1; i <=numero; i++) {
-
+            
             System.out.println(i);
+
+            for (int e = 1; e <=i; e++){
+
+                System.out.print(e);
+                
+                
+            }
+
 }
 
         scan.close();
